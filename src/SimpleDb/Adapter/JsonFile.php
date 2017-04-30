2753d2b1c9e4280b0eb43b6812740d3d2c7b1cad
Karl Xu <karl.yogi.xu@gmail.com>
2017-04-30 14:02:49 +0800

    add expection

--- src/SimpleDb/Adapter/JsonFile.php
+++ src/SimpleDb/Adapter/JsonFile.php
@@ -2,7 +2,29 @@
 
 namespace TestDemo\SimpleDb\Adapter;
 
-class JsonFile
+class JsonFile implements AdapterInterface
 {
+
+    protected $file;
+
+    public function __construct($file)
+    {
+        if(!file_exists($file))
+            throw new \InvalidArgumentException('File does not exist.');
+        $this->file = $file;
+    }
+
+    public function read()
+    {
+        
+        $contents = file_get_contents($this->file);
+
+        return json_decode($contents, true);
+
+    }
+
+    public function write(array $data)
+    {
+    }
 
 }
--- tests/Adapter/JsonFileTest.php
+++ tests/Adapter/JsonFileTest.php
@@ -10,7 +10,7 @@ class JsonFileTest extends TestCase
 
     public function setUp()
     {
-        $this->object = new JsonFile();
+        $this->object = new JsonFile('/tmp/data.json');
     }
 
 
@@ -20,5 +20,27 @@ class JsonFileTest extends TestCase
     public function implementsAdapterInterface()
     {
         $this->assertInstanceOf(AdapterInterface::class, $this->object);
+    }
+
+    /**
+     * @test
+     */
+    public function canReadJsonData()
+    {
+        $jsonData = '{"foo": "bar"}';
+        file_put_contents('/tmp/data.json', $jsonData);
+
+        $data = $this->object->read();
+        $expected = array('foo' => 'bar');
+        $this->assertEquals($expected, $data);
+    }
+
+    /**
+     * @test
+     * @expectedException InvalidArgumentException
+     */
+    public function throwAnExceptionWhenFileDoNotExist()
+    {
+        $this->object = new JsonFile('/tmp/filedoesnotexist');
     }
 }
